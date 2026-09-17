4828a9532fcb03f18d3758dc2708f6b161cb06ab
Eljhon-25 <TevesEljhonNeil@icloud.com>
2026-09-17 15:33:33 +0800

    Initial commit - Next.js tutorial blog

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+ 
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Choosing Data Analytics as my IT specialization stemmed from a simple realization: raw data is modern currency, but it is useless without someone to translate it into action. I chose this path because I wanted to operate right at the intersection of technical problem-solving and strategic decision-making.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
